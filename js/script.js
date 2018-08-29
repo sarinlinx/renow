7b0b7cd3a4cb99effbe2cc5f3bd6b04ef4516009
Sarin Linx <sarinlinx@gmail.com>
2018-08-29 16:59:04 -0400

    adjusted js for nav menu scroll colors

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 $(function() {
     var header = $(".navbar");
+    var navlink = $(".nav-link");
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
         if (scroll >= 50) {
-            header.addClass("scrolled");
+            header.addClass("scrolled") && navlink.addClass("nav-link-style");
         } else {
-            header.removeClass("scrolled");
+            header.removeClass("scrolled") && navlink.removeClass("nav-link-style");
         }
     });
 
